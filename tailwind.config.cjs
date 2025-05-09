2e720a33c048038983d3436b5e9aaeb55a3f5ef8
Ochieng Ian Magus Otieno <manduli369@gmail.com>
2025-05-09 18:13:55 +0300

    Initial commit of my personal website

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = {
+  darkMode: 'class',
+  content: ['./src/**/*.{astro,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
